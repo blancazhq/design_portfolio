812d94050b3437c352ca0e54a4cd8e3f84544bcf
Huiqi Zhou <blancazhou2011@gmail.com>
2017-03-20 08:48:10 -0400

    added gutter to

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+$(window).load(function()
+{
+  $('.grid').masonry({
+  itemSelector: '.grid-item',
+  gutter: 5
+  });
+});
+
 $(document).ready(function(){
   $('.grid').masonry({
   itemSelector: '.grid-item',
